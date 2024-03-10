849a0d62b979ed5587fa82b65027220e550d8ad4
royaly <81520338+royaly-dev@users.noreply.github.com>
2024-03-10 11:42:36 +0100

    update 2.1.1

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -25,7 +25,7 @@ async function setBackground(theme) {
     let background
     let body = document.body;
     body.className = theme ? 'dark global' : 'light global';
-    if (fs.existsSync(`${__dirname}/assets/images/background/easterEgg`) && Math.random() < 0.005) {
+    if (fs.existsSync(`${__dirname}/assets/images/background/easterEgg`) && Math.random() < 0.05) {
         let backgrounds = fs.readdirSync(`${__dirname}/assets/images/background/easterEgg`);
         let Background = backgrounds[Math.floor(Math.random() * backgrounds.length)];
         background = `url(./assets/images/background/easterEgg/${Background})`;
